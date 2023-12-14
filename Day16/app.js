e6d63a58abcecabb8b33e6ca9eb167e277693689
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-14 17:43:19 +0300

    let-var-const

--- Day16/app.js
+++ Day16/app.js
@@ -6,3 +6,20 @@
 // console.warn("Bu Bir uyaridir")
 // console.error("Bu Sayfa Bulunamadi")
 // console.clear()
+
+//*! Değişkenler 
+
+// Var
+// var name = "Ahmet Cingi";
+// console.log(name);
+
+// name = "amed"
+// console.log(name);
+// var => Global ve değişebilen veri
+// let => Local ve değişmez veri
+// const = Global ve içeriği değiştirilebilen veri .push vs
+//*! Mülakatta gelir a ya tekrar değer ataması yapılmaz sadece değeri güncellenir
+// const a = [1,2,3,4,5]
+// console.log(a);
+// a = [1,2,3,4,5]
+// console.log(a);
