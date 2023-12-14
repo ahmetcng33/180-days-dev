ece7a49faa54d2d796f60ee0a2a1ac2aeb1fcda6
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-14 17:32:10 +0300

    js başlıyo

--- Day16/app.js
+++ Day16/app.js
@@ -1,1 +1,8 @@
-alert("Javascripte hoş geldiniz")
+// console.log("Merhaba JS")
+// console.log(21313214);
+// console.log(true);
+// console.log(false);
+
+// console.warn("Bu Bir uyaridir")
+// console.error("Bu Sayfa Bulunamadi")
+// console.clear()
