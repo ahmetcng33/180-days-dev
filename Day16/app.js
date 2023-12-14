2992255fdcbbe934ce80a20348547e27dfa2fcb2
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-14 18:32:48 +0300

    oto-veridönüstürme

--- Day16/app.js
+++ Day16/app.js
@@ -24,47 +24,46 @@
 // a = [1,2,3,4,5]
 // console.log(a);
 
-let value;
+
 
 // Veri Tiplerini String'e Cevirme
 
-value = 123;
-value = String(123)
-value = (123).toString()
-value = String(3.14)
-value = (3.14).toString()
-value = String(true)
-value = String(false)
-value = String(function() {console.log()})
-value = String([1,2,3,4,5])
+// let value;
+// value = 123;
+// value = String(123)
+// value = (123).toString()
+// value = String(3.14)
+// value = (3.14).toString()
+// value = String(true)
+// value = String(false)
+// value = String(function() {console.log()})
+// value = String([1,2,3,4,5])
 
 
-console.log(value);
-console.log(typeof value);
+// console.log(value);
+// console.log(typeof value);
 
 // Veritiplerini Sayilara Cevirme
 
-value = Number("123")
-value = Number(null)
-value = Number(undefined)  //Nan olur ne sıfır ne bi sayı
-value = Number("Hi everyone") //Nan 
-value = Number(function() {console.log()})
-value = Number([1,2,3,4,5])
-value = Number("3.14")
-value = parseFloat("3.14")
-value = parseInt("3") 
+// value = Number("123")
+// value = Number(null)
+// value = Number(undefined)  //Nan olur ne sıfır ne bi sayı
+// value = Number("Hi everyone") //Nan 
+// value = Number(function() {console.log()})
+// value = Number([1,2,3,4,5])
+// value = Number("3.14")
+// value = parseFloat("3.14")
+// value = parseInt("3") 
 
 // Oto Çevirme
-const a = "hello" + 34 // string + number string olur
-console.log(a);
+// const a = "hello" + 34 // string + number string olur
+// console.log(a);
 
-let b = "34" + 53  //ikisini de string yapar
-console.log(b);
+// let b = "34" + 53  //ikisini de string yapar
+// console.log(b);
+
+// console.log(value);
+// console.log(typeof(value));
 
 
-
-
-
-console.log(value);
-console.log(typeof(value));
 
