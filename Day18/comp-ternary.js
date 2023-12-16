bbaca2ae41ec766cee4777a6c33b00fdd5119efc
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-16 17:05:40 +0300

    karsılastırma-operatörleri

---
+++ Day18/comp-ternary.js
@@ -0,0 +1,26 @@
+//*!Karsilastirma Operatörleri
+
+// ==    değerleri eşit mi
+// ===   hem değer hem tipleri eşit mi
+//*  !=   eşit değil mi 
+//*  !===  hem değerleri hem tipleri eşit değil mi
+// <     küçüktür
+// >    büyüktür
+// >=   büyük eşit
+// <=   küçük eşit
+
+console.log(2==2); //true
+console.log("js"==="java"); //false türleri eşit ama değeri aynı değil
+
+console.log(2 == "2"); //true değerleri eşit
+console.log(2 === "2"); //false değeleri eşit ama türleri farklı
+
+console.log(4>2); //true 4 2 den büyük 
+console.log(4<2); //true 4 2 den küçük 
+
+console.log(2>=2); //true 2 2 den büyük eşit  
+
+
+//*! Mantıksal Bağlaç
+
+// Not operatör
