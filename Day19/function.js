d0128e0dea40110919da82b81e75f9c9475169b1
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-19 17:49:20 +0300

    IIFE

--- Day19/function.js
+++ Day19/function.js
@@ -11,6 +11,37 @@
 // merhaba("Kadir")
 
 
+// function square(x){
+//     return x*x;
+// }
+
+// function cube(x){
+//     return x*x*x;
+//     // return den sonrası çalışmaz bro boşa deneme
+//     // console.log("naber ");
+// }
+
+// let a = cube(square(12));
+
+// console.log(a);
 
 
+// function merhaba(){
+//     return "Merhaba"
+// }
 
+// console.log(merhaba());
+
+//*! Function Expression
+
+// const merhaba = function(name){
+//     console.log("merhaba " + name);
+// }
+
+// merhaba("Amed")
+
+//*! Immediately Invoked Function Expression (IIFE) - Tanımlandığı yerde çalışan fonksiyon
+
+(function(name){
+    console.log("Merhaba " + name);
+})("Amed")
