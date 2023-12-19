2b703146ab10233fffb7f21419937b15468aac8a
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-19 17:53:30 +0300

    database

--- Day19/function.js
+++ Day19/function.js
@@ -42,6 +42,27 @@
 
 //*! Immediately Invoked Function Expression (IIFE) - Tanımlandığı yerde çalışan fonksiyon
 
-(function(name){
-    console.log("Merhaba " + name);
-})("Amed")
+// (function(name){
+//     console.log("Merhaba " + name);
+// })("Amed")
+
+const database = {
+    host : "localhost",
+    add: function(){
+        console.log("Eklendi");
+    },
+    get:function(){
+        console.log("Elde Edildi");
+    },
+    update:function(id){
+        console.log(`Id: ${id} Güncellendi`);
+    },
+    delete:function(id){
+        console.log(`Id: ${id} silindi`);
+    }
+}
+
+console.log(database.host);
+database.add()
+database.update(10)
+console.log(database.update);
