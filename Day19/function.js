74d0c1ea9dde0927dd6fe78adc2c1430f9661e6f
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-19 13:22:31 +0300

    function1

---
+++ Day19/function.js
@@ -0,0 +1,16 @@
+// Fonksiyon Tanimlama
+
+// function merhaba(name = "Bilgi Yok",age= "Bilgi yok"){
+//     console.log(`Isim : ${name} Yas: ${age}`);
+// }
+
+// merhaba("Ahmet",30);  //Function call - Fonksiyon çağrısı
+
+// merhaba();
+
+// merhaba("Kadir")
+
+
+
+
+
