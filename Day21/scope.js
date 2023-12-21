94dd70a61ef8d164604ef4608f5150729561c3e3
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-21 14:58:22 +0300

    scope

---
+++ Day21/scope.js
@@ -0,0 +1,39 @@
+// Scope Kavramı
+
+
+// Global Scope
+
+// function a(){
+//     //Function Scope
+// }
+
+// if(){
+//     // Blok Scope
+// }
+
+// var value1 = 10;
+// let value2 = 20;
+// const value3 = 30;
+
+// console.log(value1,value2,value3);
+// function Func(){
+//     var value1 = 40
+//     let value2 = 50
+//     const value3 = 60
+//     console.log(value1,value2,value3);
+// }
+
+// Func()
+
+// if(true){
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a,b,c);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
