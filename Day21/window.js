fafeb7921a0f1aa8a555286936ee0f56acaa5459
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-21 14:21:44 +0300

    window.blablabla

--- Day21/window.js
+++ Day21/window.js
@@ -4,13 +4,51 @@
 
 // alert("Merhaba")
 
-const cevap = confirm("Emin Misiniz") //cevap olarak true false sonuç döner
+// const cevap = confirm("Emin Misiniz") //cevap olarak true false sonuç döner
 
-console.log(cevap);
+// console.log(cevap);
 
-if(cevap){
-    console.log("Silin Gitsin");
-}
-else{
-    console.log("silmeyin");
-}
+// if(cevap){
+//     console.log("Silin Gitsin");
+// }
+// else{
+//     console.log("silmeyin");
+// }
+
+// const cevap = prompt("2 + 2 = ?")
+
+// if(cevap == 4){
+//     console.log("Doğru");
+// }
+// else{
+//     console.log("Yanliş");
+// }
+// console.log(cevap);
+
+let value
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+
+// if(confirm("Sayfa Yenilensin mi")){
+//     window.location.reload()
+// }
+// else{
+//     console.log("Sayfa Yenilenmedi");
+// }
+
+// value = window.outerHeight;
+// value = window.outerWidth;
+
+
+// value = window.innerHeight
+// value = window.innerHeight
+
+value = window.scrollX
+value = window.scrollY
+console.log(value);
