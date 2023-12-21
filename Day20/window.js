98e48c5a992aac86c9f024f404ccbb6bd575016f
ahmetcng33 <ahmetcingi333@gmail.com>
2023-12-21 14:09:59 +0300

    window komutları

---
+++ Day20/window.js
@@ -0,0 +1,3 @@
+// alert("Merhaba")
+
+console.log(this);
---
+++ Day21/window.js
@@ -0,0 +1,16 @@
+// console.log(window.alert());
+
+//! Alert
+
+// alert("Merhaba")
+
+const cevap = confirm("Emin Misiniz") //cevap olarak true false sonuç döner
+
+console.log(cevap);
+
+if(cevap){
+    console.log("Silin Gitsin");
+}
+else{
+    console.log("silmeyin");
+}
