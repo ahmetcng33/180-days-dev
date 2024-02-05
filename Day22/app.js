751954a26e55ad693007bf138e5aa35170b1694f
ahmetcng33 <ahmetcingi333@gmail.com>
2024-02-05 11:36:12 +0300

    dom2

--- Day22/app.js
+++ Day22/app.js
@@ -31,17 +31,54 @@
 // })
 
 
-value = document.all
-value = document.body;
-value = document.head;
-value = document.location;
-value = document.location.hostname;
-value = document.location.port;
+// value = document.all
+// value = document.body;
+// value = document.head;
+// value = document.location;
+// value = document.location.hostname;
+// value = document.location.port;
 
-value = document.URL;
+// value = document.URL;
 
-value = document.characterSet;
+// value = document.characterSet;
+// console.log(value);
+
+
+
+let value;
+
+value = document;
+
+// Scriptler
+
+// value = document.scripts;
+
+// value = document.scripts.length;
+// value = document.scripts[0];
+
+// Linkler
+
+// value = document.links;
+// value = document.links[0];
+// value = document.links[document.links.length-1]; //Son Link
+// value = document.links[document.links.length-1].getAttribute("class"); //class özelliklerini yazdır
+// value = document.links[document.links.length-1].className; //class özeliklerini yazdır
+// value = document.links[document.links.length-1].classList; //class özeliklerini yazdır
+
+
+// Formlar 
+
+value = document.forms;
+value = document.forms.length;
+value = document.forms[0];
+value = document.forms["form"]
+value = document.forms[0].id;  //id özelliğini öğrenme
+value = document.forms[0].name;  //name özelliğini öğrenme
+
+value = document.forms[0].method; //Methodunu öğrenme
+
+
+
+
+
 console.log(value);
-
-
-
