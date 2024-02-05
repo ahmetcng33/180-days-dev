cf28c3286cb4be89f7f5f2b8414c11ec55ddbf36
ahmetcng33 <ahmetcingi333@gmail.com>
2024-02-05 11:27:35 +0300

    document1

---
+++ Day22/app.js
@@ -0,0 +1,47 @@
+// Window Object
+
+// console.log(window);
+// console.log(this.document);  //index.htmli görürüz
+// console.log(document);  //İndex.htmli görürüz üsttekiyle aynı ağacın kökü documenttır
+
+
+// let value;
+// value = document;
+// value = document.length; //Element sayısını verir
+// value = document.all[0];
+// value = document.all[6];
+// value = document.all[document.all.length-1];   //sonuncu elemanı verir
+
+// console.log(value);
+
+
+// const elements = document.all;
+// console.log(elements); 
+// for(let i = 0; i < elements.length; i++)
+//     console.log(elements[i]);
+
+// elements.forEach(function(element) => {
+//     console.log(element);    
+// });
+
+// const collections = Array.from(document.all);
+
+// collections.forEach(function(collection){
+//     console.log(collection);
+// })
+
+
+value = document.all
+value = document.body;
+value = document.head;
+value = document.location;
+value = document.location.hostname;
+value = document.location.port;
+
+value = document.URL;
+
+value = document.characterSet;
+console.log(value);
+
+
+
